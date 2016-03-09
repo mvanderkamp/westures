2381aea8a3d9d30d58f659c19ed05ddd870f6587
Mike Schultz <mschultz@zingchart.com>
2016-03-09 14:52:51 -0800

    Exposed internal limits as parameters for Swipe

--- src/gestures/Swipe.js
+++ src/gestures/Swipe.js
@@ -8,10 +8,10 @@ import state from './../core/state.js';
 import util from './../core/util.js';
 
 const DEFAULT_INPUTS = 1;
-const MAX_REST_TIME = 100;
-const ESCAPE_VELOCITY = 0.2;
-const TIME_DISTORTION = 100;
-const MAX_PROGRESS_STACK = 10;
+const DEFAULT_MAX_REST_TIME = 100;
+const DEFAULT_ESCAPE_VELOCITY = 0.2;
+const DEFAULT_TIME_DISTORTION = 100;
+const DEFAULT_MAX_PROGRESS_STACK = 10;
 
 /**
  * A swipe is defined as input(s) moving in the same direction in an relatively increasing velocity and leaving the screen
@@ -23,8 +23,13 @@ class Swipe extends Gesture {
 
   /**
    * Constructor function for the Swipe class.
+   * @param {Number} numInputs - The number of inputs to trigger a Swipe can be variable, and the maximum number being a factor of the browser.
+   * @param {Number} maxRestTime - The maximum resting time a point has between it's last move and current move events.
+   * @param {Number} escapeVelocity - The minimum velocity the input has to be at to emit a swipe.
+   * @param {Number} timeDistortion - A value of time in milliseconds to distort between events.
+   * @param {Number} maxProgressStack -The maximum amount of move events to keep track of for a swipe.
    */
-  constructor(numInputs) {
+  constructor(numInputs, maxRestTime, escapeVelocity, timeDistortion, maxProgressStack) {
     super();
     /**
      * The type of the Gesture
@@ -38,7 +43,34 @@ class Swipe extends Gesture {
      */
     this.numInputs = (numInputs) ? numInputs : DEFAULT_INPUTS;
 
-    //TODO: Allow maxRestTime, escapeVelocity, and timeDistortion to be modifiable.
+    /**
+     * The maximum resting time a point has between it's last move and current move events.
+     * @type {Number}
+     */
+    this.maxRestTime = (maxRestTime) ? maxRestTime : DEFAULT_MAX_REST_TIME;
+
+    /**
+     * The minimum velocity the input has to be at to emit a swipe. This is useful for determining the difference between
+     * a swipe and a pan gesture.
+     * @type {number}
+     */
+    this.escapeVelocity = (escapeVelocity) ? escapeVelocity : DEFAULT_ESCAPE_VELOCITY;
+
+    /**
+     * A value of time in milliseconds to distort between events. Browsers do not accurately measure time with the Date constructor
+     * in milliseconds, so consecutive events sometimes display the same timestamp but different x/y coordinates. This will distort
+     * a previous time in such cases by the timeDistortion's value.
+     * @type {number}
+     */
+    this.timeDistortion = (timeDistortion) ? timeDistortion : DEFAULT_TIME_DISTORTION;
+
+    /**
+     * The maximum amount of move events to keep track of for a swipe. This helps give a more accurate estimate of the user's
+     * velocity.
+     * @type {number}
+     */
+    this.maxProgressStack = (maxProgressStack) ? maxProgressStack : DEFAULT_MAX_PROGRESS_STACK;
+
   }
 
   /**
@@ -62,7 +94,7 @@ class Swipe extends Gesture {
         y: input.current.y
       });
 
-      if (progress.length > MAX_PROGRESS_STACK) {
+      if (progress.length > this.maxProgressStack) {
         progress.moves.shift();
       }
     }
@@ -72,7 +104,7 @@ class Swipe extends Gesture {
   /*move*/
 
   /**
-   * Determines if the input's history validates a swipe motion. Determines if it did not come to a complete stop (MAX_REST_TIME),
+   * Determines if the input's history validates a swipe motion. Determines if it did not come to a complete stop (maxRestTime),
    * and if it had enough of a velocity to be considered (ESCAPE_VELOCITY).
    * @param {Array} inputs - The array of Inputs on the screen
    * @returns {null} - null if the gesture is not to be emitted, Object with information otherwise.
@@ -84,10 +116,10 @@ class Swipe extends Gesture {
 
     if (progress.moves.length > 2) {
 
-      //Return if the input has not moved in MAX_REST_TIME ms.
+      //Return if the input has not moved in maxRestTime ms.
       var currentMove = progress.moves.pop();
 
-      if ((new Date().getTime()) - currentMove.time > MAX_REST_TIME) {
+      if ((new Date().getTime()) - currentMove.time > this.maxRestTime) {
         return null;
       }
  
@@ -107,18 +139,19 @@ class Swipe extends Gesture {
       //If the date is REALLY unreliable, we apply a time distortion to the last event.
       if (!lastMove) {
         lastMove = progress.moves.pop();
-        lastMove.time += TIME_DISTORTION;
+        lastMove.time += this.timeDistortion;
       }
 
       var velocity = util.getVelocity(lastMove.x, lastMove.y, lastMove.time,
         currentMove.x, currentMove.y, currentMove.time);
 
-      if (velocity > ESCAPE_VELOCITY) {
+      if (velocity > this.escapeVelocity) {
         return {
           velocity: velocity
         };
       }
     }
+
     return null;
   }
   /*end*/
