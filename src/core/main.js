384572302d7f5090ddd07411fbd9f9d9f2610c06
mjv761 <michael.vanderkamp@usask.ca>
2018-08-27 12:46:41 -0600

    Remove useless main.js file.

--- src/core/main.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file main.js
- * Main file to setup event listeners on the document,
- * and to expose the ZingTouch object
- */
-
-const ZingTouch = require('./../ZingTouch.js');
-window.ZingTouch = ZingTouch;
