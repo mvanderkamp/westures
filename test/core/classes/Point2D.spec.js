3d882a38d5e6131e913c0617cfd5492cfba82d63
mjv761 <michael.vanderkamp@usask.ca>
2018-08-30 10:37:34 -0600

    Add more tests to the Point2D test suite

--- test/core/classes/Point2D.spec.js
+++ test/core/classes/Point2D.spec.js
@@ -8,38 +8,69 @@
 
 const Point2D = require('../../../src/core/classes/Point2D.js');
 
-let origin;
-beforeEach(() => {
-  origin = new Point2D(0,0);
-});
+describe('Constructor', () => {
+  test('does not throw an error given valid input', () => {
+    expect(() => new Point2D(0,0)).not.toThrow();
+  });
 
-/** @test {add} */
-describe('add(point)', () => {
-  test('returns the addition of two points', () => {
-    const pt = new Point2D(42,5);
-    const res = origin.add(pt);
-    expect(res).toMatchObject(pt);
-    
-    const dbl = res.add(pt);
-    expect(dbl).toMatchObject({x: 84, y: 10});
+  test('returns a valid instance', () => {
+    expect(new Point2D(0,0)).toBeInstanceOf(Point2D);
+  });
+
+  test('Defaults to the origin (0,0) if no parameters given', () => {
+    expect(new Point2D()).toMatchObject({x:0, y:0});
+  });
+
+  test('Uses the given parameters', () => {
+    expect(new Point2D(42,-89)).toMatchObject({ x:42, y:-89 });
   });
 });
 
-/** @test {distanceTo} */
-describe('distanceTo', () => {
-  test('gives the correct distance', () => {
-    const pt = new Point2D(3,4);
-    expect(origin.distanceTo(pt)).toBe(5);
-    expect(origin.distanceTo(origin)).toBe(0);
-  });
-});
+describe('Prototype methods', () => {
+  let origin;
 
-/** @test {getAngle} */
-describe('getAngle', () => {
-  test('gives the correct angle', () => {
-    const pt = new Point2D(3,3);
-    expect(origin.angleTo(pt)).toBeCloseTo(Math.PI/4);
-    expect(origin.angleTo(origin)).toBe(0);
+  beforeEach(() => {
+    origin = new Point2D(0,0);
+  });
+
+  describe('add(point)', () => {
+    test('returns the addition of two points', () => {
+      const pt = new Point2D(42,5);
+      const res = origin.add(pt);
+      expect(res).toMatchObject(pt);
+
+      const dbl = res.add(pt);
+      expect(dbl).toMatchObject({x: 84, y: 10});
+    });
+  });
+
+  describe('angleTo(point)', () => {
+    test('gives the correct angle', () => {
+      const pt = new Point2D(3,3);
+      expect(origin.angleTo(pt)).toBeCloseTo(Math.PI/4);
+      expect(origin.angleTo(origin)).toBe(0);
+    });
+  });
+
+  describe('clone()', () => {
+    test('Returns a clone of the given point', () => {
+      const pt = origin.clone();
+      expect(pt).toMatchObject(origin);
+      expect(pt === origin).toBe(false);
+
+      const off = new Point2D(42, -89);
+      const cln = off.clone();
+      expect(cln).toMatchObject(off);
+      expect(cln === off).toBe(false);
+    });
+  });
+
+  describe('distanceTo(point)', () => {
+    test('gives the correct distance', () => {
+      const pt = new Point2D(3,4);
+      expect(origin.distanceTo(pt)).toBe(5);
+      expect(origin.distanceTo(origin)).toBe(0);
+    });
   });
 });
 
