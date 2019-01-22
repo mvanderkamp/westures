549a9094b930cdba20e450b37e2d3301a3490c89
mvanderkamp <michael.vanderkamp@usask.ca>
2019-01-22 15:29:37 -0600

    Clean up Rotate, remains active for >2 inputs
    But, only uses the first two inputs. At least until I figure out the
    math properly...

--- src/Rotate.js
+++ src/Rotate.js
@@ -3,14 +3,12 @@
  * Contains the Rotate class
  */
 
-// const { Gesture } = require('westures-core');
-const { Gesture } = require('../../westures-core');
+const { Gesture } = require('westures-core');
 
 const REQUIRED_INPUTS = 2;
 
 /**
- * A Rotate is defined as two inputs moving about a circle, maintaining a
- * relatively equal radius.
+ * A Rotate is defined as two inputs moving with a changing angle between them.
  *
  * @class Rotate
  */
@@ -23,8 +21,7 @@ class Rotate extends Gesture {
   }
 
   /**
-   * Initialize the progress of the gesture.  Only runs if the number of active
-   * inputs is the expected amount.
+   * Initialize the progress of the gesture.
    *
    * @param {State} input status object
    */
@@ -36,8 +33,6 @@ class Rotate extends Gesture {
     const angle = active[0].currentAngleTo(active[1]);
     const progress = active[0].getProgressOfGesture(this.id);
     progress.previousAngle = angle;
-    progress.distance = 0;
-    progress.change = 0;
   }
 
   /**
@@ -48,41 +43,35 @@ class Rotate extends Gesture {
    * @return {null}
    */
   start(state) {
-    const pivot = this.initializeProgress(state);
+    this.initializeProgress(state);
   }
 
   /**
-   * Event hook for the move of a gesture. Obtains the midpoint of two the two
-   * inputs and calculates the projection of the right most input along a unit
-   * circle to obtain an angle. This angle is compared to the previously
-   * calculated angle to output the change of distance, and is compared to the
-   * initial angle to output the distance from the initial angle to the current
-   * angle.
+   * Event hook for the move of a Rotate gesture.
    *
    * @param {State} input status object
    *
    * @return {null} - null if this event did not occur
    * @return {Object} obj.angle - The current angle along the unit circle
-   * @return {Object} obj.distanceFromOrigin - The angular distance travelled
-   *    from the initial right most point.
-   * @return {Object} obj.distanceFromLast - The change of angle between the
-   *    last position and the current position.
+   * @return {Object} obj.pivot - The pivot point of the rotation
+   * @return {Object} obj.delta - The change in angle since the last emitted
+   *                              move.
    */
   move(state) {
     const active = state.getInputsNotInPhase('end');
-    if (active.length !== REQUIRED_INPUTS) return null;
+    if (active.length < REQUIRED_INPUTS) return null;
 
     const pivot = active[0].currentMidpointTo(active[1]);
     const angle = active[0].currentAngleTo(active[1]);
 
     const progress = active[0].getProgressOfGesture(this.id);
-    progress.change = angle - progress.previousAngle;
+    const delta = angle - progress.previousAngle;
     progress.previousAngle = angle;
 
     return {
       angle,
       pivot,
-      delta: progress.change,
+      delta,
     };
   }
   /* move*/
