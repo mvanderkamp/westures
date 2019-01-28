e68790ce914c22798ae715cf4b615f87c0e878f4
mjv761 <michael.vanderkamp@usask.ca>
2019-01-28 11:48:16 -0600

    Clean up and refine Swivel gesture

--- src/Swivel.js
+++ src/Swivel.js
@@ -8,53 +8,77 @@
 const { Gesture } = require('westures-core');
 
 const REQUIRED_INPUTS = 1;
+const defaults = Object.freeze({
+  deadzoneRadius: 10,
+});
 
 /**
  * A Swivel is a single input rotating around a fixed point. The fixed point is
  * determined by the input's location at its 'start' phase.
  */
 class Swivel extends Gesture {
-  constructor(deadzoneRadius = 10) {
+  constructor(options = {}) {
     super('swivel');
-    this.deadzoneRadius = deadzoneRadius;
+
+    /**
+     * The radius around the start point in which to do nothing.
+     */
+    this.deadzoneRadius = options.deadzoneRadius || defaults.deadzoneRadius;
+
+    /**
+     * If this is set, gesture will only respond to events where this property
+     * is truthy. Should be one of 'ctrlKey', 'altKey', or 'shiftKey'.
+     */
+    this.enableKey = options.enableKey;
   }
 
+  /**
+   * Returns whether this gesture is currently enabled.
+   */
+  enabled(event) {
+    return !this.enableKey || event[this.enableKey];
+  }
+
+  /**
+   * Event hook for the start of a Swivel gesture.
+   */
   start(state) {
-    const started = state.getInputsInPhase('start')[0];
-    const progress = started.getProgressOfGesture(this.id);
-    const current = started.current;
-    const point = current.point;
-    const event = current.originalEvent;
-    if (event.ctrlKey) {
-      progress.pivot = point;
-    }
+    if (!this.enabled(state.event)) return null;
+
+    const started = state.getInputsInPhase('start');
+    if (started.length !== REQUIRED_INPUTS) return null;
+
+    const progress = started[0].getProgressOfGesture(this.id);
+    progress.pivot = started[0].current.point;
   }
 
+  /**
+   * Event hook for the move of a Swivel gesture.
+   */
   move(state) {
-    const active = state.getInputsNotInPhase('end');
-    if (active.length === 1) {
-      const input = active[0];
+    if (state.active.length !== REQUIRED_INPUTS) return null;
+
+    const input = state.active[0];
+
+    const progress = input.getProgressOfGesture(this.id);
+    if (this.enabled(state.event) && progress.pivot) {
+      const point = input.current.point;
+      const pivot = progress.pivot;
+      const angle = pivot.angleTo(point);
+      let delta = 0;
+      if (progress.hasOwnProperty('previousAngle')) {
+        delta = angle - progress.previousAngle;
+      }
+      progress.previousAngle = angle;
 
       if (input.totalDistanceIsWithin(this.deadzoneRadius)) {
         return null;
-      }
-
-      const event = input.current.originalEvent;
-      const progress = input.getProgressOfGesture(this.id);
-      if (event.ctrlKey && progress.pivot) {
-        const point = input.current.point;
-        const pivot = progress.pivot;
-        const angle = pivot.angleTo(point);
-        let delta = 0;
-        if (progress.hasOwnProperty('previousAngle')) {
-          delta = angle - progress.previousAngle;
-        }
-        progress.previousAngle = angle;
-        return { delta, pivot, point };
       } else {
-        // CTRL key was released, therefore pivot point is now invalid.
-        delete progress.pivot;
+        return { delta, pivot, point };
       }
+    } else {
+      // CTRL key was released, therefore pivot point is now invalid.
+      delete progress.pivot;
     }
   }
 }
