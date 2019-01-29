5c3322dfdbdf2e86741e1346916a9cdb0e0ab95b
mjv761 <michael.vanderkamp@usask.ca>
2019-01-28 18:32:30 -0600

    Tweak Swivel to allow re-enabling

--- src/Swivel.js
+++ src/Swivel.js
@@ -50,6 +50,7 @@ class Swivel extends Gesture {
 
     const progress = started[0].getProgressOfGesture(this.id);
     progress.pivot = started[0].current.point;
+    progress.previousAngle = 0;
   }
 
   /**
@@ -61,17 +62,21 @@ class Swivel extends Gesture {
     const input = state.active[0];
 
     const progress = input.getProgressOfGesture(this.id);
-    if (this.enabled(state.event) && progress.pivot) {
+    if (this.enabled(state.event)) {
+      if (!progress.pivot) {
+        // Restart: enableKey was just pressed again.
+        progress.pivot = input.current.point;
+        progress.previousAngle = 0;
+        return null;
+      }
+
       const point = input.current.point;
       const pivot = progress.pivot;
       const angle = pivot.angleTo(point);
-      let delta = 0;
-      if (progress.hasOwnProperty('previousAngle')) {
-        delta = angle - progress.previousAngle;
-      }
+      const delta = angle - progress.previousAngle;
       progress.previousAngle = angle;
 
-      if (input.totalDistanceIsWithin(this.deadzoneRadius)) {
+      if (pivot.distanceTo(point) <= this.deadzoneRadius) {
         return null;
       } else {
         return { delta, pivot, point };
