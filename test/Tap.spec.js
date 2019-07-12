5c81eef126a118e566ce2820ab2f66ab2f799df1
mjv761 <michael.vanderkamp@usask.ca>
2019-07-11 23:49:51 -0600

    Refactor Tap test to use arrow functions

--- test/Tap.spec.js
+++ test/Tap.spec.js
@@ -7,22 +7,21 @@
 const Tap = require('../src/Tap.js');
 
 describe('Tap', function() {
-  test('should be instantiated', function() {
+  test('should be instantiated', () => {
     expect(Tap).toBeTruthy();
   });
 
-  test('should return a Tap object.', function() {
+  test('should return a Tap object.', () => {
     expect(new Tap()).toBeInstanceOf(Tap);
   });
 
-  test('should return accept delay and number of inputs as parameters',
-    function() {
-      let _tap = new Tap({
-        maxDelay: 2000,
-        numInputs: 2,
-      });
-      expect(_tap.maxDelay).toEqual(2000);
-      expect(_tap.numInputs).toEqual(2);
+  test('should return accept delay and number of inputs as parameters', () => {
+    let _tap = new Tap({
+      maxDelay: 2000,
+      numInputs: 2,
     });
+    expect(_tap.maxDelay).toEqual(2000);
+    expect(_tap.numInputs).toEqual(2);
+  });
 });
 
