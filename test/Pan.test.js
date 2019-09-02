b65a4a4497971384d34a2041d22789d7b517196f
mjv761 <michael.vanderkamp@usask.ca>
2019-09-02 16:02:09 -0600

    Set up Pan tests

--- test/Pan.test.js
+++ test/Pan.test.js
@@ -2,11 +2,23 @@
  * Tests Pan class
  */
 
-/* global expect, describe, test */
+/* global expect, describe, test, jest, beforeEach, beforeAll */
 
 'use strict';
 
 const Pan = require('src/Pan.js');
+const { Point2D } = require('westures-core');
+
+let element = null;
+let handler = null;
+
+beforeAll(() => {
+  element = document.createElement('div');
+});
+
+beforeEach(() => {
+  handler = jest.fn();
+});
 
 describe('Pan', () => {
   describe('constructor', () => {
@@ -16,9 +28,57 @@ describe('Pan', () => {
   });
 
   describe('phase hooks', () => {
+    let pan = null;
+    let state = null;
+
+    beforeAll(() => {
+      state = {
+        centroid: new Point2D(42, 117),
+      };
+    });
+
+    beforeEach(() => {
+      pan = new Pan(element, handler);
+    });
+
+    describe('start(state)', () => {
+      test('Returns null', () => {
+        expect(pan.start(state)).toBeFalsy();
+      });
+    });
+
+    describe('move(state)', () => {
+    });
+
+    describe('end(state)', () => {
+      test('Returns null', () => {
+        expect(pan.start(state)).toBeFalsy();
+      });
+    });
+
+    describe('cancel(state)', () => {
+      test('Returns null', () => {
+        expect(pan.start(state)).toBeFalsy();
+      });
+    });
   });
 
   describe('other prototype methods', () => {
+    let pan = null;
+    beforeEach(() => {
+      pan = new Pan(element, handler);
+    });
+
+    describe('restart(state)', () => {
+      const state = {
+        centroid: new Point2D(42, 117),
+      };
+
+      test('Resets the previous value', () => {
+        pan.restart(state);
+        expect(pan.previous).toBe(state.centroid);
+      });
+    });
   });
 });
 
