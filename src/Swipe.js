2fca331cdb1ccfa143264d2463a5c6bdecb70ff5
Michael van der Kamp <michael.vanderkamp@usask.ca>
2022-02-05 15:23:38 -0600

    Use static methods instead of inner
    This way they are accessible for testing

--- src/Swipe.js
+++ src/Swipe.js
@@ -24,76 +24,6 @@ const MS_THRESHOLD = 300;
  */
 
 /**
- * Calculates the angle of movement along a series of moves.
- *
- * @inner
- * @memberof westures.Swipe
- * @see {@link https://en.wikipedia.org/wiki/Mean_of_circular_quantities}
- *
- * @param {{time: number, point: westures-core.Point2D}} moves - The moves list
- * to process.
- * @param {number} vlim - The number of moves to process.
- *
- * @return {number} The angle of the movement.
- */
-function calc_angle(moves, vlim) {
-  const point = moves[vlim].point;
-  let sin = 0;
-  let cos = 0;
-  for (let i = 0; i < vlim; ++i) {
-    const angle = moves[i].point.angleTo(point);
-    sin += Math.sin(angle);
-    cos += Math.cos(angle);
-  }
-  sin /= vlim;
-  cos /= vlim;
-  return Math.atan2(sin, cos);
-}
-
-/**
- * Local helper function for calculating the velocity between two timestamped
- * points.
- *
- * @inner
- * @memberof westures.Swipe
- *
- * @param {object} start
- * @param {westures-core.Point2D} start.point
- * @param {number} start.time
- * @param {object} end
- * @param {westures-core.Point2D} end.point
- * @param {number} end.time
- *
- * @return {number} velocity from start to end point.
- */
-function velocity(start, end) {
-  const distance = end.point.distanceTo(start.point);
-  const time = end.time - start.time + 1;
-  return distance / time;
-}
-
-/**
- * Calculates the veloctiy of movement through a series of moves.
- *
- * @inner
- * @memberof westures.Swipe
- *
- * @param {{time: number, point: westures-core.Point2D}} moves - The moves list
- * to process.
- * @param {number} vlim - The number of moves to process.
- *
- * @return {number} The velocity of the moves.
- */
-function calc_velocity(moves, vlim) {
-  let max = 0;
-  for (let i = 0; i < vlim; ++i) {
-    const current = velocity(moves[i], moves[i + 1]);
-    if (current > max) max = current;
-  }
-  return max;
-}
-
-/**
  * A swipe is defined as input(s) moving in the same direction in an relatively
  * increasing velocity and leaving the screen at some point before it drops
  * below it's escape velocity.
@@ -171,7 +101,7 @@ class Swipe extends Gesture {
     }
 
     this.saved = null;
-    return this.validate(result);
+    return Swipe.validate(result);
   }
 
   cancel() {
@@ -189,8 +119,8 @@ class Swipe extends Gesture {
     }
     const vlim = PROGRESS_STACK_SIZE - 1;
     const { point, time } = this.moves[vlim];
-    const velocity = calc_velocity(this.moves, vlim);
-    const direction = calc_angle(this.moves, vlim);
+    const velocity = Swipe.calc_velocity(this.moves, vlim);
+    const direction = Swipe.calc_angle(this.moves, vlim);
     const centroid = point;
     return { point, velocity, direction, time, centroid };
   }
@@ -198,12 +128,78 @@ class Swipe extends Gesture {
   /**
    * Validates that an emit should occur with the given data.
    *
+   * @static
    * @param {?ReturnTypes.SwipeData} data
    * @returns {?ReturnTypes.SwipeData}
    */
-  validate(data) {
+  static validate(data) {
     if (data == null) return null;
     return (Date.now() - data.time > MS_THRESHOLD) ? null : data;
+  }
+
+  /**
+   * Calculates the angle of movement along a series of moves.
+   *
+   * @static
+   * @see {@link https://en.wikipedia.org/wiki/Mean_of_circular_quantities}
+   *
+   * @param {{time: number, point: westures-core.Point2D}} moves - The moves
+   * list to process.
+   * @param {number} vlim - The number of moves to process.
+   *
+   * @return {number} The angle of the movement.
+   */
+  static calc_angle(moves, vlim) {
+    const point = moves[vlim].point;
+    let sin = 0;
+    let cos = 0;
+    for (let i = 0; i < vlim; ++i) {
+      const angle = moves[i].point.angleTo(point);
+      sin += Math.sin(angle);
+      cos += Math.cos(angle);
+    }
+    sin /= vlim;
+    cos /= vlim;
+    return Math.atan2(sin, cos);
+  }
+
+  /**
+   * Local helper function for calculating the velocity between two timestamped
+   * points.
+   *
+   * @static
+   * @param {object} start
+   * @param {westures-core.Point2D} start.point
+   * @param {number} start.time
+   * @param {object} end
+   * @param {westures-core.Point2D} end.point
+   * @param {number} end.time
+   *
+   * @return {number} velocity from start to end point.
+   */
+  static velocity(start, end) {
+    const distance = end.point.distanceTo(start.point);
+    const time = end.time - start.time + 1;
+    return distance / time;
+  }
+
+  /**
+   * Calculates the veloctiy of movement through a series of moves.
+   *
+   * @static
+   * @param {{time: number, point: westures-core.Point2D}} moves - The moves
+   * list to process.
+   * @param {number} vlim - The number of moves to process.
+   *
+   * @return {number} The velocity of the moves.
+   */
+  static calc_velocity(moves, vlim) {
+    let max = 0;
+    for (let i = 0; i < vlim; ++i) {
+      const current = Swipe.velocity(moves[i], moves[i + 1]);
+      if (current > max) max = current;
+    }
+    return max;
   }
 }
 
